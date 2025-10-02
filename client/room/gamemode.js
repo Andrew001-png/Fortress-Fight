fb9d25431840c82bd8299773ab41f65a43d845d5
Andrew <139539083+Andrew001-png@users.noreply.github.com>
2025-10-03 01:32:10 +0900

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -1,1 +1,12 @@
-.
+// Импортируем то, что нам надо
+import * as Room from 'pixel_combats/room';
+import * as Basic from 'pixel_combats/basic';
+import * as Library from './library.js';
+
+// Константы
+const gm=Room.GameMode.Parameters, props=Room.Properties.GetContext();
+
+// Импортируем команды
+Library.Create_team();
+
+// Создаем таблицу лидеров
