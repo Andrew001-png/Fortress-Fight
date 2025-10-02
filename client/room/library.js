f801420958322387175be526ead23208b101279f
Andrew <139539083+Andrew001-png@users.noreply.github.com>
2025-10-03 01:36:10 +0900

    Update library.js

--- client/room/library.js
+++ client/room/library.js
@@ -1,1 +1,12 @@
- 
+// Импортируем то, что надо
+import * as Room from 'pixel_combats/room';
+import * as Basic from 'pixel_combats/Basic';
+
+// Функция создания команд
+export function Create_teams(){
+
+Room.Teams.Add("Blue", "<b><size=13><color=cyan>Синие</a></size></b>", new Basic.Color(0,0,1,0));
+
+
+
+};
